f4df5801c6f4de496e4c9de621edd64e8fb5872f
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2026-06-21 21:39:07 -0400

    fixing the delete endpoint

--- backend/userclub/userclub.py
+++ backend/userclub/userclub.py
@@ -8,8 +8,8 @@ class UserClub(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key=True, nullable=False)
     club_id = db.Column(db.Integer, db.ForeignKey('clubs.club_id'), primary_key=True, nullable=False)
     joined_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
-    user = db.relationship('User', backref='user_clubs')
-    club = db.relationship('Club', backref='user_clubs')
+    user = db.relationship('User', backref=db.backref('user_clubs', cascade='all, delete-orphan'))
+    club = db.relationship('Club', backref=db.backref('user_clubs', cascade='all, delete-orphan'))
 
     def __repr__(self):
         return f'<UserClub user_id={self.user_id} club_id={self.club_id}>'
