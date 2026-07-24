8c40d38a4d9fe0da11117790b5be55868205cacd
Jade <jade03@my.yorku.ca>
2026-07-23 22:31:06 -0400

    Manually cherry-pick changes from this commit: a2dcc43e

---
+++ backend/migrations/versions/2468f5ea582a_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: 2468f5ea582a
+Revises: 
+Create Date: 2026-07-22 05:06:27.428924
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2468f5ea582a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
